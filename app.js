b1af26673eadb1f3302f08dc32e4e6554a4c5322
phil-ward <ward.phil@gmail.com>
2026-02-24 10:29:46 +0100

    Add Color Transition and User Dimension Choice
    Create button to trigger redraw of squares based on user defined counts
    Dynamically resize squares to fit within same container sizing
    Validate upper limit for user input
    Add CSS transition for etch-a-sketch effects

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-const containerDiv = document.querySelector('.container')
-const NUMBER_OF_SQUARES = 256
+const containerDiv = document.querySelector(".container");
+const button = document.querySelector("button");
+let total_squares = 256;
+buildSquare(Math.sqrt(total_squares));
+button.addEventListener("click", promptUser);
 
+function promptUser() {
+  let squaresPerSide = prompt(
+    "How many squares per side?\nPlease enter a number less than 100",
+    "",
+  );
+  if (squaresPerSide >= 100) {
+    let squaresPerSide = prompt(
+      "How many squares per side?\nPlease enter a number less than 100",
+      "",
+    );
+  }
+  buildSquare(squaresPerSide);
+}
 
-for (i=0; i < NUMBER_OF_SQUARES; i++) {
-    let squareDiv = document.createElement('div')
-    squareDiv.classList.add("square-div")
-    squareDiv.classList.add("square-div:hover")
-    containerDiv.append(squareDiv)
+function buildSquare(squaresPerSide) {
+  total_squares = squaresPerSide ** 2;
+  while (containerDiv.firstChild) {
+    containerDiv.removeChild(containerDiv.firstChild);
+  }
+  squareDimensions = 960 / squaresPerSide;
+  console.log(`${squareDimensions}px`);
+  for (i = 0; i < total_squares; i++) {
+    let squareDiv = document.createElement("div");
+    squareDiv.classList.add("square-div");
+    squareDiv.classList.add("square-div:hover");
+    squareDiv.style.height = `${squareDimensions}px`;
+    squareDiv.style.width = `${squareDimensions}px`;
+    containerDiv.append(squareDiv);
+  }
 }
